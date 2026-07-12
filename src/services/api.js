a4b2ffca64eda1d1c7401e8c4ab3ba6072434fc1
Mueez Irshad <irshadmoeez10@gmail.com>
2026-07-12 07:45:13 +0500

    ädd backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://final-hachatone.vercel.app/",
+  baseURL: "https://final-hachatone.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
