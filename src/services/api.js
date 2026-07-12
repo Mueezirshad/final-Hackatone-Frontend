b8b7f0690a20dc47fa54e07549bcf8cafcae4743
Mueez Irshad <irshadmoeez10@gmail.com>
2026-07-12 09:04:13 +0500

    add url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://final-hachatone.vercel.app/api",
+  baseURL: "https://final-hachatone.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
